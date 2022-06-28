55c1d3f208af7e0785865976596fbe849881467f
ali-shahKust <syed.ali.shah3938@gmail.com>
2022-06-28 12:30:55 +0500

    ejs implementation

--- app.js
+++ app.js
@@ -2,22 +2,21 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs');
     app.listen(3000);
 //fetching HTML Pages
     app.get('/',(req,res)=>{
-    res.sendFile('./views/index.html',{root:__dirname});
+    res.render('index',{title:'Home'}); 
     });
 
     app.get('/about',(req,res)=>{
-        res.sendFile('./views/about.html',{root:__dirname});
+        res.render('about',{title:"About"});
         });
-
-    app.get('/about-us',(req,res)=>{
-            res.redirect('/about');
-            });
-
+        app.get('/create',(req,res)=>{
+            res.render('create',{title:"New Blog"});
+        });
     //404 page not found 
     //Must be at the end of the code
     app.use((req,res)=>{    
-        res.status(404).sendFile('./views/404.html',{root:__dirname});  
+        res.status(404).render('404',{title:"Not Found"});  
     })
